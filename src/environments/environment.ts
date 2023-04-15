d4233439c072564b446ab0427056a86fb4603886
Konrad M <10982159+konmacie@users.noreply.github.com>
2023-04-15 11:54:58 +0200

    enable debug logging

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,12 @@
+import { LogLevel, LoggerType } from 'src/app/core/services/logger.service';
+
 export const environment = {
   production: false,
   apiUrl: 'http://localhost:8000/',
+  logging: [
+    {
+      type: LoggerType.CONSOLE,
+      level: LogLevel.DEBUG,
+    },
+  ],
 };
